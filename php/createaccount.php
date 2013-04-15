a263eabaf0c28416288d7e439a3df39785945a4c
David <davidng0123@live.com>
2013-04-14 21:06:24 -0700

    implemented setPassword()

--- php/createaccount.php
+++ php/createaccount.php
@@ -3,21 +3,35 @@
 	Predis\Autoloader::register();
 	
 	$redis = new Predis\Client('tcp://kong.idlemonkeys.net:6379');
-	//$redis->set('mwuser:12345', 'cake');
+	//$redis->set('user:12345', 'cake');
+	
+	$uuid = generateUUID();
+	//test values
+	$password = 'cakecake';
 	
 	function generateUUID() {
 		return randomString(8) . '-' . randomString(4) . '-4' . randomString(3) . '-8' . randomString(3) . '-' . randomString(12);
 	}
 	
-	function randomString($length) {
-		$chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+	function randomString($len) {
+		$chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 		$charlen = strlen($chars);
 		$rand = "";
-		for ($i = 0; $i < $length; $i++) {
+		for ($i = 0; $i < $len; $i++) {
 			$rand .= $chars[rand(0, $charlen - 1)];
 		}
 		return $rand;
 	}
 	
-	echo generateUUID();
+	function setPassword($id, $pw, $db) {
+		$salt = randomString(8);
+		$hash = crypt($pw, $salt);
+		$passwordEntry = array(
+			'pass' => $hash,
+			'salt' => $salt
+		);
+		$db->set('user:password:' . $id, json_encode($passwordEntry));
+	}
+	
+	setPassword($uuid, $password, $redis);
 ?>
