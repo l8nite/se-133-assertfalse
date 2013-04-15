d4960be8c8c5dc3378aec48b6115643e2db043da
David <davidng0123@live.com>
2013-04-14 20:28:19 -0700

    implemented generateUUID()

--- php/createaccount.php
+++ php/createaccount.php
@@ -1,3 +1,23 @@
 ﻿<?php
-	echo "cake";
+	require 'Predis/Autoloader.php';
+	Predis\Autoloader::register();
+	
+	$redis = new Predis\Client('tcp://kong.idlemonkeys.net:6379');
+	//$redis->set('mwuser:12345', 'cake');
+	
+	function generateUUID() {
+		return randomString(8) . '-' . randomString(4) . '-4' . randomString(3) . '-8' . randomString(3) . '-' . randomString(12);
+	}
+	
+	function randomString($length) {
+		$chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+		$charlen = strlen($chars);
+		$rand = "";
+		for ($i = 0; $i < $length; $i++) {
+			$rand .= $chars[rand(0, $charlen - 1)];
+		}
+		return $rand;
+	}
+	
+	echo generateUUID();
 ?>
