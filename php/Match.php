de714a124a55f44aeca7c0ec652705d047c54e20
David <davidng0123@live.com>
2013-04-21 13:02:48 -0700

    implemented word filtering based on dictionary

---
+++ php/Match.php
@@ -0,0 +1,28 @@
+﻿<?php
+class Match {
+
+	///**
+	// * Constructor.
+	// *
+	// * @param
+	// */
+	//public function __construct() {
+	//}
+	
+	/**
+	 *
+	 */
+	public static function filter($string) {
+		$dictionary = array("i","am");
+		$split = preg_split("/[.,:;\s\!\?-]+/", $string);
+		$keywords = array();
+		
+		foreach($split as $word) {
+			if (!in_array(strtolower($word), $dictionary))
+				array_push($keywords, $word);
+		}
+		
+		return $keywords;
+	}
+}
+?>
