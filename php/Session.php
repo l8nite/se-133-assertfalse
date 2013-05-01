bfd0e2344c570e34fc4b2a0fe326b2e665f8f61a
David <davidng0123@live.com>
2013-04-30 19:11:54 -0700

    commented Session.php

--- php/Session.php
+++ php/Session.php
@@ -2,7 +2,11 @@
 class Session {
 
 	/**
+	 * Maps a user's session key or session ID to their UUID.
 	 *
+	 * @param connection $db Redis connection object.
+	 * @param string $sid User's session ID.
+	 * @return string User's UUID.
 	 */
 	public static function resolveSessionID($db, $sid) {
 		$sessionEntry = json_decode($db->get('session:user:' . $sid));
@@ -10,7 +14,11 @@ class Session {
 	}
 
 	/**
+	 * Creates a new session and session ID for a user.
 	 *
+	 * @param connection $db Redis connection object.
+	 * @param string $id User's UUID.
+	 * @return string Random session ID that maps to user.
 	 */
 	public static function generateSession($db, $id) {
 		$sid = self::generateSessionID();
@@ -29,14 +37,19 @@ class Session {
 	}
 
 	/**
+	 * Generates a new, random session ID.
 	 *
+	 * @return string Session ID.
 	 */
 	private static function generateSessionID() {
 		return self::randomString(8) . '-' . self::randomString(4) . '-4' . self::randomString(3) . '-9' . self::randomString(3) . '-' . self::randomString(12);
 	}
 
 	/**
+	 * Generates a random string of specified length.
 	 *
+	 * @param integer $len Desired string length.
+	 * @return string Random string.
 	 */
 	private static function randomString($len) {
 		$chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
