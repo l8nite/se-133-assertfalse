977b21fbd31fe0b7a6a3f3fa645412fee663f6be
Shaun Guth <shaun.guth@gmail.com>
2013-05-03 19:45:53 +0400

    remove old navbar code

--- include/header.php
+++ include/header.php
@@ -58,7 +58,7 @@ if ($session->isLoggedIn())
 
         <div class="navbar navbar-inverse">
             <div class="navbar-inner">
-                <a class="brand" href="#">MentorWeb</a>
+                <a class="brand" href="/index.php">MentorWeb</a>
                 <ul class="nav">
                 <?php
                     $pages = array(
@@ -105,41 +105,4 @@ if ($session->isLoggedIn())
             </div>
         </div>
 
-
-<?php
-/*
-        <div class="navbar-wrapper">
-            <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
-            <div class="container">
-                <div class="navbar navbar-inverse">
-                    <div class="navbar-inner">
-                        <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
-                        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-
-                        <a class="brand" href="index.php">MentorWeb</a>
-
-                        <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
-                        <div class="nav-collapse collapse">
-                            <ul class="nav">
-                                <li class="active"><a href="#">Home</a></li>
-                                <li><a href="#about">About</a></li>
-                            <?php
-                                if($isLoggedIn == false):
-                            ?>
-                                <li><div><a class="btn btn-success" href="signin.php">Sign In</a></div>
-                            <?php else: ?>
-                                <a class="btn btn-success" href="SignOut_script.php">Sign Out</a>
-                            <?php endif; ?>
-                            </ul>
-                        </div><!--/.nav-collapse -->
-                    </div><!-- /.navbar-inner -->
-                </div><!-- /.navbar -->
-            </div> <!-- /.container -->
-        </div><!-- /.navbar-wrapper -->
-
-*/?>
 <!-- end of header.php included content -->
