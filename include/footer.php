ced9ff068c62b8a3f1246d7ee416a4e6c3e9c52b
Shaun Guth <shaun.guth@gmail.com>
2013-05-03 11:40:06 +0400

    footer.php closes html properly

---
+++ include/footer.php
@@ -0,0 +1,15 @@
+      <hr>
+
+      <footer>
+        <p>&copy; MentorWeb 2013</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
