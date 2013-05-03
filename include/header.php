b5072193f5e8803ee518fbe54e1c1910e3e96aa5
Shaun Guth <shaun.guth@gmail.com>
2013-05-03 12:46:38 +0400

    move styles out of header.php

--- include/header.php
+++ include/header.php
@@ -1,116 +1,62 @@
-<?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', True);
-
-    require 'Session.php';
-    require 'SignIn_v2.php';
-    require 'database.php';
-
-    $isLoggedIn = SignIn::enforceSignIn();
-?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Sign in &middot; MentorWeb</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+        <meta charset="utf-8">
 
-    <!-- Le styles -->
-    <link href="styles/bootstrap.css" rel="stylesheet">
-    <link href="styles/bootstrap-responsive.css" rel="stylesheet">
-    <link href="styles/mentorweb.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 40px;
-        padding-bottom: 40px;
-      }
+        <title><?php echo $pageTitle || 'MentorWeb' ?></title>
 
-      .form-signin {
-        max-width: 300px;
-        padding: 19px 29px 29px;
-        margin: 75px auto 20px;
-        background-color: #fff;
-        border: 1px solid #e5e5e5;
-        -webkit-border-radius: 5px;
-           -moz-border-radius: 5px;
-                border-radius: 5px;
-        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                box-shadow: 0 1px 2px rgba(0,0,0,.05);
-      }
-      .form-signin .form-signin-heading,
-      .form-signin .checkbox {
-        margin-bottom: 10px;
-      }
-      .form-signin input[type="text"],
-      .form-signin input[type="password"] {
-        font-size: 16px;
-        height: auto;
-        margin-bottom: 15px;
-        padding: 7px 9px;
-      }
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    </style>
-    <link href="styles/bootstrap-responsive.css" rel="stylesheet">
+        <link href="styles/bootstrap.css" rel="stylesheet">
+        <link href="styles/bootstrap-responsive.css" rel="stylesheet">
+        <link href="styles/mentorweb.css" rel="stylesheet">
+    </head>
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="scripts/html5shiv.js"></script>
-    <![endif]-->
+    <body>
+        <div class="navbar-wrapper">
+            <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+            <div class="container">
+                <div class="navbar navbar-inverse">
+                    <div class="navbar-inner">
+                        <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+                        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
-  </head>
+                        <a class="brand" href="index.php">MentorWeb</a>
 
-  <body>
-    <!-- NAVBAR
-    ================================================== -->
-    <div class="navbar-wrapper">
-        <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
-        <div class="container">
+                        <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+                        <div class="nav-collapse collapse">
+                            <ul class="nav">
+                                <li class="active"><a href="#">Home</a></li>
+                                <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="./profile-edit.php">Edit Profile</a></li>
+                                        <li><a href="#">Upgrade to Premium</a></li>
+                                        <li class="divider"></li>
+                                        <li class="nav-header">Mentors</li>
+                                        <li><a href="findamentor.php">Find a Mentor</a></li>
+                                        <li><a href="#">Review a Mentor</a></li>
+                                        <li><a href="#">Manage Mentors</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#about">About</a></li>
+                            </ul>
+                            <?php
+                                if($isLoggedIn == false):
+                            ?>
+                                <a class="btn btn-success" href="signin.php">Sign In</a>
+                            <?php else: ?>
+                                <a class="btn btn-success" href="SignOut_script.php">Sign Out</a>
+                            <?php endif; ?>
+                        </div><!--/.nav-collapse -->
+                    </div><!-- /.navbar-inner -->
+                </div><!-- /.navbar -->
+            </div> <!-- /.container -->
+        </div><!-- /.navbar-wrapper -->
 
-        <div class="navbar navbar-inverse">
-            <div class="navbar-inner">
-            <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
-            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="brand" href="./home.php">MentorWeb</a>
-            <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
-            <div class="nav-collapse collapse">
-                <ul class="nav">
-                <li class="active"><a href="#">Home</a></li>
-                <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                    <li><a href="./profile-edit.php">Edit Profile</a></li>
-                    <li><a href="#">Upgrade to Premium</a></li>
-                        <li class="divider"></li>
-                    <li class="nav-header">Mentors</li>
-                    <li><a href="./findamentor.php">Find a Mentor</a></li>
-                    <li><a href="#">Review a Mentor</a></li>
-                    <li><a href="#">Manage Mentors</a></li>
-                    </ul>
-                </li>
-                <li><a href="#about">About</a></li>
-                </ul>
-				<?php if($isLoggedIn == false): ?>
-                <a class="btn btn-success" href="./signin.php">Sign In</a>
-
-                <?php else: ?>
-                 <a class="btn btn-success" href="../../php/SignIn/SignOut_script.php">Sign Out</a>
-				<?php endif; ?>
-            </div><!--/.nav-collapse -->
-            </div><!-- /.navbar-inner -->
-        </div><!-- /.navbar -->
-        </div> <!-- /.container -->
-    </div><!-- /.navbar-wrapper -->
+<!-- end of header.php included content -->
