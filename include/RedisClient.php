6aa207a59c45b76ae14c5917cc8c3920c9ec0215
Shaun Guth <shaun.guth@gmail.com>
2013-05-03 12:36:52 +0400

    throw exception on construction of RedisClient

--- include/RedisClient.php
+++ include/RedisClient.php
@@ -1,5 +1,30 @@
 <?php
-    require 'Predis/Autoloader.php';
-    Predis\Autoloader::register();
-    $redis = new Predis\Client('tcp://kong.idlemonkeys.net:6379');
+/**
+ * Factory method which returns a single instance of the Predis client object
+ */
+require_once 'Predis/Autoloader.php';
+
+class RedisClient
+{
+    /**
+    * Returns a connected instance of the predis client
+    * Multiple calls to this function will return the same result
+    */
+    public static function GetConnectedPredisInstance()
+    {
+        static $predis = null;
+
+        if ($predis === null) {
+            Predis\Autoloader::register();
+            $predis = new Predis\Client('tcp://kong.idlemonkeys.net:6379');
+        }
+
+        return $predis;
+    }
+
+    public function __construct()
+    {
+        throw new Exception("RedisClient should not be constructed.");
+    }
+}
 ?>
