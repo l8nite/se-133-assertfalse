212ed7924dc1da933ca96a8ada4de980432f1374
Shaun Guth <shaun.guth@gmail.com>
2013-05-04 11:40:54 +0400

    add dump to home page for now

--- include/pages/home.php
+++ include/pages/home.php
@@ -3,4 +3,12 @@
 This page gets loaded when the user is logged in and visits the home page
  */
 ?>
-User's home page goes here
+<p>
+This is the user's home page.
+</p>
+<p>
+I'm not really sure what should go here, so here's a dump of your user profile data.
+</p>
+<textarea style="width: 50%; height: 800px;">
+<?php var_dump($user->getDetails()); ?>
+</textarea>
