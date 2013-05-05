4b90ec6fa3e1c823fa2d5eba71b0e054637ddbfa
John <dingodongo1@gmail.com>
2013-05-05 15:52:04 -0700

    Created homepage tutorial

--- include/pages/home.php
+++ include/pages/home.php
@@ -3,12 +3,32 @@
 This page gets loaded when the user is logged in and visits the home page
  */
 ?>
-<p>
-This is the user's home page.
-</p>
-<p>
-I'm not really sure what should go here, so here's a dump of your user profile data.
-</p>
-<textarea style="width: 50%; height: 800px;">
-<?php var_dump($user->getDetails()); ?>
-</textarea>
+<h1>Welcome To MentorWeb!</h1>
+<p>Since this is your first time logging in, let me introduce the functionalities of MentorWeb to get you going quickly.</p>
+<div class="row" style="margin-bottom: 20px;" >
+	<div class="span4 text-center" style="margin-top: 40px;"><h1 style="font-size: 100px;">1</h1></div>
+	<div class="span8"><h3>Introduce Yourself</h3>
+		<p>In order for MentorWeb to have sufficient information to join you with the correct Mentor, you should complete your profile. This will allow MentorWeb to properly select available Mentors that will suit your needs.</p> 
+		<button class="btn btn-primary" href="./edit-profile.php"><i class="icon-user"></i>  Update Profile</button>
+	</div>
+</div>
+<div class="row  well">
+	<div class="span8"><h3>Find a Mentor</h3>
+		<p>Now it is time for you to search for a suitable mentor. Using an advanced specialized algorithm, MentorWeb combines your mined profile information and your selected keywords to recommend your top mentors.</p>
+		<p>The search results will allow you to open a connect request with the mentor. Once the mentor has accepted your request, you are ready to begin communication.</p>
+		<button class="btn btn-primary" href="./findamentor.php"><i class="icon-thumbs-up"></i>  Search for a Mentor</a>
+	</div>	
+	<div class="span3 text-center" style="margin-top: 40px;"><h1 style="font-size: 100px;">2</h1></div>
+</div>
+<div class="row">
+	<div class="span4 text-center" style="margin-top: 40px;"><h1 style="font-size: 100px;">3</h1></div>
+	<div class="span8"><h3>Communication is key!</h3>
+		<p>Now that your mentor is available, it is time to start communicating. Communication can be done through our proprietary messaging framework to have live chats with your mentor.</p>
+		<button class="btn btn-primary" href="./messages.php"><i class="icon-comment"></i>  Start Messaging</button>
+	</div>
+</div>
+
+
+
+
+
