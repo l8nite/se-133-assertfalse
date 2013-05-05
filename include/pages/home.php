30018efa58653bce8473f221c354ab0f5c120894
John <dingodongo1@gmail.com>
2013-05-05 15:58:40 -0700

    Update links again

--- include/pages/home.php
+++ include/pages/home.php
@@ -9,14 +9,14 @@ This page gets loaded when the user is logged in and visits the home page
 	<div class="span4 text-center" style="margin-top: 40px;"><h1 style="font-size: 100px;">1</h1></div>
 	<div class="span8"><h3>Introduce Yourself</h3>
 		<p>In order for MentorWeb to have sufficient information to join you with the correct Mentor, you should complete your profile. This will allow MentorWeb to properly select available Mentors that will suit your needs.</p> 
-		<button class="btn btn-primary" href="edit-profile.php"><i class="icon-user"></i>  Update Profile</button>
+		<a class="btn btn-primary" href="edit-profile.php"><i class="icon-user"></i>  Update Profile</a>
 	</div>
 </div>
 <div class="row  well">
 	<div class="span8"><h3>Find a Mentor</h3>
 		<p>Now it is time for you to search for a suitable mentor. Using an advanced specialized algorithm, MentorWeb combines your mined profile information and your selected keywords to recommend your top mentors.</p>
 		<p>The search results will allow you to open a connect request with the mentor. Once the mentor has accepted your request, you are ready to begin communication.</p>
-		<button class="btn btn-primary" href="findamentor.php"><i class="icon-thumbs-up"></i>  Search for a Mentor</a>
+		<a class="btn btn-primary" href="findamentor.php"><i class="icon-thumbs-up"></i>  Search for a Mentor</a>
 	</div>	
 	<div class="span3 text-center" style="margin-top: 40px;"><h1 style="font-size: 100px;">2</h1></div>
 </div>
@@ -24,7 +24,7 @@ This page gets loaded when the user is logged in and visits the home page
 	<div class="span4 text-center" style="margin-top: 40px;"><h1 style="font-size: 100px;">3</h1></div>
 	<div class="span8"><h3>Communication is key!</h3>
 		<p>Now that your mentor is available, it is time to start communicating. Communication can be done through our proprietary messaging framework to have live chats with your mentor.</p>
-		<button class="btn btn-primary" href="messages.php"><i class="icon-comment"></i>  Start Messaging</button>
+		<a class="btn btn-primary" href="messages.php"><i class="icon-comment"></i>  Start Messaging</a>
 	</div>
 </div>
 
